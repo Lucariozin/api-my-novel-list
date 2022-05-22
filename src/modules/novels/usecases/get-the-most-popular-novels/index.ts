eaae7ed413529ddc0af8355166215b591c0668ea
Lucas Andrade <lucasfelixthemyth@gmail.com>
2022-05-22 16:31:27 -0300

    'generate fake novels data' mock file created.

--- src/modules/novels/usecases/get-the-most-popular-novels/index.ts
+++ src/modules/novels/usecases/get-the-most-popular-novels/index.ts
@@ -1,14 +1,19 @@
+import { Novel } from '@/domain/entities/novel';
 import { NovelsRepository } from '@/infra/repositories/novels-repository/contract-novels-repository';
+
+interface Params {
+  limit?: number;
+}
 
 export class GetTheMostPopularNovelsUseCase {
   constructor(
     private readonly novelsRepository: NovelsRepository,
   ) {}
 
-  async execute() {
+  async execute({ limit = 10 }: Params): Promise<Novel[]> {
     const novels = await this.novelsRepository.getAllNovels();
 
-    const theMostPopularNovels = novels.sort((a, b) => b.stars - a.stars).splice(0, 10);
+    const theMostPopularNovels = novels.sort((a, b) => b.stars - a.stars).splice(0, limit);
 
     return theMostPopularNovels;
   }
